8ed8577930606c228dcb279d47c7facce8ecc29e
yangxiaobing0725 <2797422923@qq.com>
2015-07-21 20:45:16 +0800

    a
    a

---
+++ sbbbbb/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  sbbbbb
+//
+//  Created by kpkj-ios on 15/7/21.
+//  Copyright (c) 2015年 kpkj-ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ sbbbbb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sbbbbb
+//
+//  Created by kpkj-ios on 15/7/21.
+//  Copyright (c) 2015年 kpkj-ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
